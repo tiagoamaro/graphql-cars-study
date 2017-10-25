e377a96ba7941f0bf399a1e56bebb2e03c4c847e
Tiago Amaro <tiagopadrela@gmail.com>
2017-10-25 21:31:06 -0200

    remove unused frozen string literal magic argument

--- app/graphql/types/car_type.rb
+++ app/graphql/types/car_type.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Types
   CarType = GraphQL::ObjectType.define do
     name 'Car'
--- app/graphql/types/manufacturer_type.rb
+++ app/graphql/types/manufacturer_type.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Types
   ManufacturerType = GraphQL::ObjectType.define do
     name 'Manufacturer'
